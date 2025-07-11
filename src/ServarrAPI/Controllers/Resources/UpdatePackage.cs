4b01086affbd3de5ca133b9c77ae035484a663c6
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-07-11 21:57:24 +0300

    Replace octokit with graphql client

--- src/ServarrAPI/Controllers/Resources/UpdatePackage.cs
+++ src/ServarrAPI/Controllers/Resources/UpdatePackage.cs
@@ -8,24 +8,19 @@ namespace ServarrAPI.Controllers.Update
 
         public DateTime ReleaseDate { get; set; }
 
+        public string Branch { get; set; }
+
         public string Filename { get; set; }
 
         public string Url { get; set; }
-
-        public UpdateChanges Changes { get; set; }
 
         /// <summary>
         ///     Must be a SHA256 hash of the zip file obtained from <see cref="Url"/>.
         /// </summary>
         public string Hash { get; set; }
 
-        public string Branch { get; set; }
-
         public string Runtime { get; set; }
 
-        /// <summary>
-        ///     The Status of the build, in theory should always be "success"
-        /// </summary>
-        public string Status { get; set; }
+        public UpdateChanges Changes { get; set; }
     }
 }
